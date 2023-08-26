da5b7eb66a035074aedd589168fc117369ff56bd
Just EnginErin <31420768+just-enginerin@users.noreply.github.com>
2023-08-25 21:35:48 -0400

    Sprint 2 - APIs
    - findAllTasks API, error handling, and swagger documentation
    - createTask API, error handling, and swagger documentation

--- routes/employee.js
+++ routes/employee.js
@@ -14,16 +14,27 @@ const { ObjectId } = require('mongodb')
 
 const ajv = new Ajv() // Create a new instance of the Ajv class.
 
-//Define a schema to validate a new task.
-// TODO: Figure out why this is not preventing us from sending in additional properties.
-const TaskSchema = {
+// Category schema
+const categorySchema = {
   type: 'object',
   properties: {
-    text: {
-      type: 'string', // Property 'text' must have a string value
-    }
+    categoryName: {type: 'string'},
+    backgroundColor: {type: 'string'}
   },
-  required: ['text'], // only accept a value from the Text property
+  required: ['categoryName', 'backgroundColor'],
+  additionalProperties: true
+}
+
+
+//Define a schema to validate a new task.
+// TODO: Figure out why this is not preventing us from sending in additional properties.
+const taskSchema = {
+  type: 'object',
+  properties: {
+    text: { type: 'string' }, // Property 'text' must have a string value
+    category: categorySchema
+  },
+  required: ['text', 'category'], // only accept a value from the Text and Category properties
   additionalProperties: false // deny any additional properties from being included in the request payload.
 }
 
@@ -39,10 +50,13 @@ const TaskSchema = {
 router.get('/:empId', (req, res, next) => {
 
   try {
+
+    // Retrieve the employee ID from the request parameters.
     let { empId } = req.params
     empId = parseInt(empId, 10)
     console.log("empId:", empId)
 
+    // Validation: Enforce that the employee ID is a number.
     if (isNaN(empId)) {
       const err = new Error('Input must be a number.')
       err.status = 400
@@ -51,9 +65,11 @@ router.get('/:empId', (req, res, next) => {
       return
     }
 
+    // If validation passes, make a call to the database.
     mongo(async db => {
       const employee = await db.collection('employees').findOne({ empId })
 
+      // Display an error if the employee's data was not successfully retrieved.
       if (!employee) {
         const err = new Error('Unable to find employee with empId' + empId)
         err.status = 404
@@ -62,6 +78,7 @@ router.get('/:empId', (req, res, next) => {
         return
       }
 
+      // Otherwise, send the employee data in the response.
       res.send(employee)
     }, next)
 
@@ -151,30 +168,35 @@ router.post('/:empId/tasks', (req, res, next) => {
 
     mongo(async db => {
 
+      // Fetch an employee from the database that matches the provided employee ID.
       const employee = await db.collection('employees').findOne({ empId })
 
       console.log('Employee: ', employee)
 
+      // If a matching employee was not found, return an error.
       if(!employee) {
-        const err = new Error('Unable to find employee with empId: ', empId)
+        const err = new Error('Unable to find employee with empId: ' + empId)
         err.status = 404
         console.log('Error: ', err)
         next(err)
         return
       }
 
-      const { text } = req.body
+      // Retrieve the new task information from the request body.
+      const { task } = req.body
 
+      console.log('New Task: ', task)
       console.log('Request body: ', req.body)
 
       // Validate the request object.
-      const validator = ajv.compile(TaskSchema)
-      const valid = validator ({ text })
+      const validator = ajv.compile(taskSchema)
+      const valid = validator(task)
 
       console.log('Valid?: ', valid)
 
+      // If task validation fails, return an error.
       if(!valid) {
-        const err = new Error('Bad Request')
+        const err = new Error('Bad Request: ' + validator.errors[0].message)
         err.status = 400
         err.errors = validator.errors
         console.log('Request body validation failed: ', err)
@@ -182,14 +204,16 @@ router.post('/:empId/tasks', (req, res, next) => {
         return
       }
 
-      const task = {
+      // Build the new Task object to insert into the database.
+      const newTask = {
         _id: new ObjectId(),
-        text
+        text: task.text,
+        category: task.category
       }
 
       const result = await db.collection('employees').updateOne(
         { empId },
-        { $push: { todo: task}}
+        { $push: { todo: newTask}}
       )
 
       console.log('Result: ', result)
@@ -203,7 +227,7 @@ router.post('/:empId/tasks', (req, res, next) => {
         return
       }
 
-      res.status(201).send({ id: task._id })
+      res.status(201).send({ id: newTask._id })
 
     }, next)
 
